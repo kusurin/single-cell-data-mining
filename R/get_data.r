d3e2aa9dd5878dcd9eb613a0b3228baf40c7a600
kusurin <kusu_rin@sjtu.edu.cn>
2024-05-26 22:16:26 +0800

    style:

--- R/get_data.r
+++ R/get_data.r
@@ -1,19 +1,19 @@
-gse112302 <- getGEO('GSE112302',destdir = ".")
+gse112302 <- getGEO("GSE112302", destdir = ".")
 class(gse112302)
 length(gse112302)
-
-#以下试验，没用
 
 a <- read.table("./GSE112302_RAW/")
 
 a <- read.table("./GSE84465_GBM_All_data.csv.gz")
+b <- read.table("./SraRunTable.txt", sep = ",", header = T)
 
-b <- read.table("./SraRunTable.txt",sep = ",", header = T)
-
-b.group <- b[,c("plate_id","Well","TISSUE","Patient_ID")]
-b.group$sample <- paste0("X",b.group$plate_id,".",b.group$Well)
+b.group <- b[, c("plate_id", "Well", "TISSUE", "Patient_ID")]
+b.group$sample <- paste0("X", b.group$plate_id, ".", b.group$Well)
 head(b.group)
 
-index <- which(b.group$TISSUE=="Tumor")
+group <- b.group[index, ] # 筛选的是行
+head(group)
 
-a.filt <- a[,index]
+index <- which(b.group$tissue == "Tumor")
+
+a.filt <- a[, index]
