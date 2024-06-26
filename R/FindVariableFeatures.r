ca19a5b06bba1526b5e7d045014d9812417f93de
kusurin <kusu_rin@sjtu.edu.cn>
2024-06-27 00:52:26 +0800

    fix

--- R/FindVariableFeatures.r
+++ R/FindVariableFeatures.r
@@ -1,6 +1,5 @@
 scRNA <- FindVariableFeatures(scRNA, selection.method = "vst", nfeatures = 1500) 
 
-p1_3_mid <- VariableFeaturePlot(scRNA) 
 p1_3 <- LabelPoints(plot = plot1, points = head(VariableFeatures(scRNA), 10) , repel = TRUE, size=2.5) +
   theme(legend.position = c(0.1,0.8)) +
   labs(tag = "C")
